dcd251cafb39f3b2be9d64d5d74cfe4209f134fd
Lakshmi <Lakshmi@DESKTOP-6SRCAA5>
2020-06-23 16:45:30 +0530

    Updated DemoTest1 by Lakshmi

---
+++ MavenVtigerFW/src/test/java/com/vtiger/testscripts/DemoTest1.java
@@ -0,0 +1,14 @@
+package com.vtiger.testscripts;
+import org.testng.annotations.Test;
+public class DemoTest1 {
+	@Test
+	public void checkDemo()
+	{
+		System.out.println("Demo running");
+		
+		System.out.println("DemoTest Class");
+		System.out.println("demotest ends");
+		System.out.println("Democlass");
+	}
+}
+
